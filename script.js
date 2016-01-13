4a44cbafec16414edb24d6e17685d7238d7577e0
Vu Nguyen Hung <spamspam252@gmail.com>
2016-01-13 18:02:49 +0700

    Done day 3

--- script.js
+++ script.js
@@ -14,25 +14,37 @@ function readTextFile(file) {
     return temp;
 }
 
-// function day1a(text) {
-//     var result = 0;
-//     for (var i = 0; i < text.length; i++) {
-//         if (text[i] === "(") result++;
-//         else result--;
-//     }
-//     return result;
-// }
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
+
+function day1a(text) {
+    var result = 0;
+    for (var i = 0; i < text.length; i++) {
+        if (text[i] === "(") result++;
+        else result--;
+    }
+    return result;
+}
 
 
-// function day1b(text) {
-//     var result = 0;
-//     for (var i = 0; i < text.length; i++) {
-//         if (text[i] === "(") result++;
-//         else result--;
-//         if (result === -1) break;
-//     }
-//     return i + 1;
-// }
+function day1b(text) {
+    var result = 0;
+    for (var i = 0; i < text.length; i++) {
+        if (text[i] === "(") result++;
+        else result--;
+        if (result === -1) break;
+    }
+    return i + 1;
+}
 
 function day2a(text) {
     var result = 0;
@@ -43,7 +55,7 @@ function day2a(text) {
     for (var i = 0; i < lines.length; i++) {
         line = lines[i];
         // console.log(line);
-        arr = line.split("x");
+        arr = line.split('x');
         
         result += 2 * (arr[0] * arr[1] + arr[1] * arr[2] + arr[0] * arr[2]);
         result+= Math.min(arr[0] * arr[1],arr[1] * arr[2],arr[0] * arr[2]);
@@ -58,20 +70,114 @@ function day2b(text){
     var arr = new Array(3);
     var ribbon, bow;
     for(var i = 0; i < lines.length; i++){
-        arr = lines[i].split('x').sort(function(a, b){return a-b});
-        console.log(arr[0] + "," + arr[1] + "," + arr[2]);
+        arr = lines[i].split('x').sort(function(a, b){return a-b;});
+        // console.log(arr[0] + "," + arr[1] + "," + arr[2]);
         ribbon = 2 * arr[0] + 2*arr[1];
-        console.log(ribbon);
+        // console.log(ribbon);
         bow = arr[0]*arr[1]*arr[2];
-        console.log(bow);
+        // console.log(bow);
         result += (ribbon + bow);
     }
     return result;
 }
 
+function day3a(text){
+    var result = 0;
+    // var lines = text.split('\n');
+    
+    var map = createArray(999,999);
+    
+    for(var temp = 0; temp < 999; temp++)
+        for(var temp2 = 0; temp2 < 999; temp2++)
+            map[temp][temp2] = 0;
+            
+    var i = 499, j = 499;
+    map[i][j]++;
+    for(var h = 0; h < text.length; h++){
+        switch(text[h]){
+            case '>':
+                map[i][++j]++;
+                break;
+            case '<':
+                map[i][--j]++;
+                break;
+            case 'v':
+                map[++i][j]++;
+                break;
+            case '^':
+                map[--i][j]++;
+                break;
+        }
+    }
+    
+    for(temp = 0; temp < 999; temp++)
+        for(temp2 = 0; temp2 < 999; temp2++)
+            if(map[temp][temp2]> 0) result++;
+    
+    return result;
+}
+
+
+function day3b(text){
+    var result = 0;
+    // var lines = text.split('\n');
+    
+    var map = createArray(999,999);
+    
+    // var map = new Array(999);
+    // for(var temp = 0; temp < 1000; temp++)
+    //     map[temp] = new Array(999);
+        
+    for(var temp = 0; temp < 999; temp++)
+        for(var temp2 = 0; temp2 < 999; temp2++)
+            map[temp][temp2] = 0;
+            
+    var i = 499, j = 499;
+    var x = 499, y = 499;
+    map[i][j]++;
+    for(var h = 0; h < text.length; h+=2){
+        switch(text[h]){
+            case '>':
+                map[i][++j]++;
+                break;
+            case '<':
+                map[i][--j]++;
+                break;
+            case 'v':
+                map[++i][j]++;
+                break;
+            case '^':
+                map[--i][j]++;
+                break;
+        }
+        switch(text[h+1]){
+            case '>':
+                map[x][++y]++;
+                break;
+            case '<':
+                map[x][--y]++;
+                break;
+            case 'v':
+                map[++x][y]++;
+                break;
+            case '^':
+                map[--x][y]++;
+                break;
+        }
+    }
+    
+    for(temp = 0; temp < 999; temp++)
+        for(temp2 = 0; temp2 < 999; temp2++)
+            if(map[temp][temp2]> 0) result++;
+    
+    return result;
+}
+
+
+
 window.onload = function () {
     var fileUrl = "input.txt";
     var text = "";
     text = readTextFile(fileUrl);
-    document.getElementById("output").innerHTML = day2b(text).toString();
+    document.getElementById("output").innerHTML = day3a(text).toString();
 };
