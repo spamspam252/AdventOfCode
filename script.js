31812ed7314cdba154a73f88f4d9134b91b40017
Vu Nguyen Hung <spamspam252@gmail.com>
2016-01-13 21:19:51 +0700

    Done day 4

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* global CryptoJS */
+
 function readTextFile(file) {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
@@ -20,7 +22,7 @@ function createArray(length) {
 
     if (arguments.length > 1) {
         var args = Array.prototype.slice.call(arguments, 1);
-        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+        while (i--) arr[length - 1 - i] = createArray.apply(this, args);
     }
 
     return arr;
@@ -51,50 +53,50 @@ function day2a(text) {
     var lines = text.split('\n');
     var arr = new Array(3);
     var line;
-    
+
     for (var i = 0; i < lines.length; i++) {
         line = lines[i];
         // console.log(line);
         arr = line.split('x');
-        
+
         result += 2 * (arr[0] * arr[1] + arr[1] * arr[2] + arr[0] * arr[2]);
-        result+= Math.min(arr[0] * arr[1],arr[1] * arr[2],arr[0] * arr[2]);
+        result += Math.min(arr[0] * arr[1], arr[1] * arr[2], arr[0] * arr[2]);
     }
     return result;
 }
 
 
-function day2b(text){
+function day2b(text) {
     var result = 0;
     var lines = text.split('\n');
     var arr = new Array(3);
     var ribbon, bow;
-    for(var i = 0; i < lines.length; i++){
-        arr = lines[i].split('x').sort(function(a, b){return a-b;});
+    for (var i = 0; i < lines.length; i++) {
+        arr = lines[i].split('x').sort(function (a, b) { return a - b; });
         // console.log(arr[0] + "," + arr[1] + "," + arr[2]);
-        ribbon = 2 * arr[0] + 2*arr[1];
+        ribbon = 2 * arr[0] + 2 * arr[1];
         // console.log(ribbon);
-        bow = arr[0]*arr[1]*arr[2];
+        bow = arr[0] * arr[1] * arr[2];
         // console.log(bow);
         result += (ribbon + bow);
     }
     return result;
 }
 
-function day3a(text){
+function day3a(text) {
     var result = 0;
     // var lines = text.split('\n');
     
-    var map = createArray(999,999);
-    
-    for(var temp = 0; temp < 999; temp++)
-        for(var temp2 = 0; temp2 < 999; temp2++)
+    var map = createArray(999, 999);
+
+    for (var temp = 0; temp < 999; temp++)
+        for (var temp2 = 0; temp2 < 999; temp2++)
             map[temp][temp2] = 0;
-            
+
     var i = 499, j = 499;
     map[i][j]++;
-    for(var h = 0; h < text.length; h++){
-        switch(text[h]){
+    for (var h = 0; h < text.length; h++) {
+        switch (text[h]) {
             case '>':
                 map[i][++j]++;
                 break;
@@ -109,34 +111,34 @@ function day3a(text){
                 break;
         }
     }
-    
-    for(temp = 0; temp < 999; temp++)
-        for(temp2 = 0; temp2 < 999; temp2++)
-            if(map[temp][temp2]> 0) result++;
-    
+
+    for (temp = 0; temp < 999; temp++)
+        for (temp2 = 0; temp2 < 999; temp2++)
+            if (map[temp][temp2] > 0) result++;
+
     return result;
 }
 
 
-function day3b(text){
+function day3b(text) {
     var result = 0;
     // var lines = text.split('\n');
     
-    var map = createArray(999,999);
+    var map = createArray(999, 999);
     
     // var map = new Array(999);
     // for(var temp = 0; temp < 1000; temp++)
     //     map[temp] = new Array(999);
         
-    for(var temp = 0; temp < 999; temp++)
-        for(var temp2 = 0; temp2 < 999; temp2++)
+    for (var temp = 0; temp < 999; temp++)
+        for (var temp2 = 0; temp2 < 999; temp2++)
             map[temp][temp2] = 0;
-            
+
     var i = 499, j = 499;
     var x = 499, y = 499;
     map[i][j]++;
-    for(var h = 0; h < text.length; h+=2){
-        switch(text[h]){
+    for (var h = 0; h < text.length; h += 2) {
+        switch (text[h]) {
             case '>':
                 map[i][++j]++;
                 break;
@@ -150,7 +152,7 @@ function day3b(text){
                 map[--i][j]++;
                 break;
         }
-        switch(text[h+1]){
+        switch (text[h + 1]) {
             case '>':
                 map[x][++y]++;
                 break;
@@ -165,19 +167,44 @@ function day3b(text){
                 break;
         }
     }
-    
-    for(temp = 0; temp < 999; temp++)
-        for(temp2 = 0; temp2 < 999; temp2++)
-            if(map[temp][temp2]> 0) result++;
-    
+
+    for (temp = 0; temp < 999; temp++)
+        for (temp2 = 0; temp2 < 999; temp2++)
+            if (map[temp][temp2] > 0) result++;
+
     return result;
 }
 
+function day4a(text) {
+    var i = 100000;
+    var temp = "";
+    while (true) {
+        temp = text.concat(i);
+        temp = String(CryptoJS.MD5(temp));
+        if (temp.match(/^00000/gi)) break;
+        else i++;
+    }
+    return i;
+}
+function day4b(text) {
+    var i = 100000;
+    var temp = "";
+    while (true) {
+        temp = text.concat(i);
+        temp = String(CryptoJS.MD5(temp));
+        if (temp.match(/^000000/gi)) break;
+        else i++;
+    }
+    return i;
+}
 
 
 window.onload = function () {
+
     var fileUrl = "input.txt";
     var text = "";
     text = readTextFile(fileUrl);
-    document.getElementById("output").innerHTML = day3a(text).toString();
+    // document.getElementById("output").innerHTML = day4b(text).toString();
+    // alert(CryptoJS.MD5("pqrstuv1048970"));
+
 };
